4eea78d55d61cb0cd368625663ce841a1b0b6273
Abhayesh Maurya <abhayeshmaurya@gmail.com>
2018-02-14 16:07:04 +0530

    New Files Add

--- app/src/main/java/com/example/apoorvesh/marriagebell/MainActivity.java
+++ app/src/main/java/com/example/apoorvesh/marriagebell/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.apoorvesh.marriagebell;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,25 +15,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    TextView photography;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigation);
+                photography= findViewById(R.id.textview_photography);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +38,32 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.bnvendors:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        break;
+                    case R.id.bnvenues:
+                        startActivity(new Intent(getApplicationContext(),Venues.class));
+                        break;
+                    case R.id.bninspiration:
+                        startActivity(new Intent(getApplicationContext(),Inspiration.class));
+                }
+                return true;
+            }
+        });
+
+
+    }
+        public void onclick(View v){
+        switch (v.getId()){
+            case R.id.textview_photography:
+                startActivity(new Intent(MainActivity.this,Photography.class));
+
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/apoorvesh/marriagebell/Photography.java
@@ -0,0 +1,13 @@
+package com.example.apoorvesh.marriagebell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Photography extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photography2);
+    }
+}
